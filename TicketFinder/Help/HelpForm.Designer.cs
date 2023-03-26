b363c9c6a526cd3d73ee50f748003a0969c972f6
Jake <jake@bortz.com.au>
2023-03-26 15:28:09 +1100

    update Help

--- TicketFinder/Help/HelpForm.Designer.cs
+++ TicketFinder/Help/HelpForm.Designer.cs
@@ -46,6 +46,9 @@ namespace TicketFinder.Help
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -57,6 +60,8 @@ namespace TicketFinder.Help
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.tabPage5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -66,11 +71,12 @@ namespace TicketFinder.Help
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(493, 181);
+            this.tabControl1.Size = new System.Drawing.Size(514, 181);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -80,7 +86,7 @@ namespace TicketFinder.Help
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(485, 155);
+            this.tabPage1.Size = new System.Drawing.Size(506, 155);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Intro";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -92,7 +98,7 @@ namespace TicketFinder.Help
             this.label1.Location = new System.Drawing.Point(3, 3);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
-            this.label1.Size = new System.Drawing.Size(386, 149);
+            this.label1.Size = new System.Drawing.Size(407, 149);
             this.label1.TabIndex = 0;
             this.label1.Text = "This handy app will help you access Jira tickets quickly.\r\n\r\nJust open the app, e" +
     "nter the ticket number, and this will find the page for you.\r\n\r\nFirst you need t" +
@@ -103,7 +109,7 @@ namespace TicketFinder.Help
             this.pictureBox1.BackgroundImage = global::TicketFinder.Properties.Resources.Clippy;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pictureBox1.Location = new System.Drawing.Point(389, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(410, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(93, 149);
             this.pictureBox1.TabIndex = 1;
@@ -116,7 +122,7 @@ namespace TicketFinder.Help
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(485, 155);
+            this.tabPage2.Size = new System.Drawing.Size(506, 155);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "How to Use";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -128,7 +134,7 @@ namespace TicketFinder.Help
             this.label2.Location = new System.Drawing.Point(96, 3);
             this.label2.Name = "label2";
             this.label2.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
-            this.label2.Size = new System.Drawing.Size(386, 149);
+            this.label2.Size = new System.Drawing.Size(407, 149);
             this.label2.TabIndex = 2;
             this.label2.Text = resources.GetString("label2.Text");
             // 
@@ -150,7 +156,7 @@ namespace TicketFinder.Help
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(485, 155);
+            this.tabPage3.Size = new System.Drawing.Size(506, 155);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Editing Buttons";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -162,18 +168,16 @@ namespace TicketFinder.Help
             this.label3.Location = new System.Drawing.Point(3, 3);
             this.label3.Name = "label3";
             this.label3.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
-            this.label3.Size = new System.Drawing.Size(386, 149);
+            this.label3.Size = new System.Drawing.Size(407, 149);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Each button has a name and a URL that it opens.\r\n\r\nPress the \"Edit\" button to acc" +
-    "ess the settings screen where you can add, edit, reorder, and delete buttons.\r\n\r" +
-    "\nEach button must have a unique name.";
+            this.label3.Text = resources.GetString("label3.Text");
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackgroundImage = global::TicketFinder.Properties.Resources.Clippy;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pictureBox3.Location = new System.Drawing.Point(389, 3);
+            this.pictureBox3.Location = new System.Drawing.Point(410, 3);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(93, 149);
             this.pictureBox3.TabIndex = 3;
@@ -186,9 +190,9 @@ namespace TicketFinder.Help
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(485, 155);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Quick launching";
+            this.tabPage4.Size = new System.Drawing.Size(506, 155);
+            this.tabPage4.TabIndex = 5;
+            this.tabPage4.Text = "Guessing URLs";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // label4
@@ -196,11 +200,11 @@ namespace TicketFinder.Help
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(96, 3);
-            this.label4.Name = "label4";
+            this.label4.Name = "label7";
             this.label4.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
-            this.label4.Size = new System.Drawing.Size(386, 149);
+            this.label4.Size = new System.Drawing.Size(407, 149);
             this.label4.TabIndex = 4;
-            this.label4.Text = resources.GetString("label4.Text");
+            this.label4.Text = resources.GetString("label7.Text");
             // 
             // pictureBox4
             // 
@@ -208,7 +212,7 @@ namespace TicketFinder.Help
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox4.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Name = "pictureBox7";
             this.pictureBox4.Size = new System.Drawing.Size(93, 149);
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
@@ -220,9 +224,9 @@ namespace TicketFinder.Help
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(485, 155);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "History";
+            this.tabPage5.Size = new System.Drawing.Size(506, 155);
+            this.tabPage5.TabIndex = 3;
+            this.tabPage5.Text = "Quick launching";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // label5
@@ -230,28 +234,62 @@ namespace TicketFinder.Help
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(3, 3);
-            this.label5.Name = "label5";
+            this.label5.Name = "label4";
             this.label5.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
-            this.label5.Size = new System.Drawing.Size(386, 149);
-            this.label5.TabIndex = 2;
-            this.label5.Text = resources.GetString("label5.Text");
+            this.label5.Size = new System.Drawing.Size(407, 149);
+            this.label5.TabIndex = 4;
+            this.label5.Text = resources.GetString("label4.Text");
             // 
             // pictureBox5
             // 
             this.pictureBox5.BackgroundImage = global::TicketFinder.Properties.Resources.Clippy;
             this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pictureBox5.Location = new System.Drawing.Point(389, 3);
-            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Location = new System.Drawing.Point(410, 3);
+            this.pictureBox5.Name = "pictureBox4";
             this.pictureBox5.Size = new System.Drawing.Size(93, 149);
-            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabIndex = 5;
             this.pictureBox5.TabStop = false;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.label6);
+            this.tabPage6.Controls.Add(this.pictureBox6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(506, 155);
+            this.tabPage6.TabIndex = 4;
+            this.tabPage6.Text = "History";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(96, 3);
+            this.label6.Name = "label5";
+            this.label6.Padding = new System.Windows.Forms.Padding(20, 20, 20, 0);
+            this.label6.Size = new System.Drawing.Size(407, 149);
+            this.label6.TabIndex = 2;
+            this.label6.Text = resources.GetString("label5.Text");
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::TicketFinder.Properties.Resources.Clippy;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox6.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox6.Name = "pictureBox5";
+            this.pictureBox6.Size = new System.Drawing.Size(93, 149);
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
             // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 181);
+            this.ClientSize = new System.Drawing.Size(514, 181);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -270,6 +308,8 @@ namespace TicketFinder.Help
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.tabPage5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -292,5 +332,8 @@ namespace TicketFinder.Help
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
